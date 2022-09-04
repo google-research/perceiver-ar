044aa8b92c92bf3140beda2e22794d5b9b1760b1
Alex Lembcke <alex.lembcke@gmail.com>
2022-09-04 10:21:21 -0400

    Fix OOM error.

--- perceiver_ar/experiment.py
+++ perceiver_ar/experiment.py
@@ -42,6 +42,8 @@ import optax
 from perceiver_ar import perceiver_ar_model
 import tensorflow as tf
 
+tf.config.set_visible_devices([], "GPU")
+
 FLAGS = flags.FLAGS
 
 OptState = Tuple[optax.TraceState, optax.ScaleByScheduleState, optax.ScaleState]
