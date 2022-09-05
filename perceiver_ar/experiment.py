9860c2c2465e8b0bc9430556f0715847d0848fd5
Alex Lembcke <alex.lembcke@gmail.com>
2022-09-05 19:05:30 -0400

    Updated comment.

--- perceiver_ar/experiment.py
+++ perceiver_ar/experiment.py
@@ -42,7 +42,7 @@ import optax
 from perceiver_ar import perceiver_ar_model
 import tensorflow as tf
 
-# Ensure TensorFlow doesn't use entire GPU
+# Ensure TensorFlow doesn't use GPU.
 tf.config.set_visible_devices([], "GPU")
 
 FLAGS = flags.FLAGS
